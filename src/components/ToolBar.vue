225923801b74fb74ea9f4172876ece626c80002a
coffeeRang <rla78ehd@gmail.com>
2019-07-22 17:24:06 +0900

    vue propject used by vuetify first commit

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Admin</span>
+        <span class="font-weight-light">_BackOffice</span>
+      </v-toolbar-title>
+      <v-spacer>
+        
+      </v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+  
+</template>
+
+<script>
+export default {
+  
+};
+</script>
