98c9eb54e5caa2b47049ea708f164476e114afd3
coffeeRang <rla78ehd@gmail.com>
2019-07-22 18:10:36 +0900

    Main.vue에서 sideBar 분리한 항목 제거

--- src/layout/sideBarItems.js
+++ src/layout/sideBarItems.js
@@ -1,14 +1,24 @@
 const sideBarItems = [
-  { icon: 'contacts', text: 'Contacts' },
-  { icon: 'history', text: 'Frequently contacted' },
-  // { icon: 'content_copy', text: 'Duplicates' },
+  { icon: 'contacts', text: '사용자관리 메뉴' },
+  { icon: 'history', text: '사이트 이력' },
+  { icon: 'g_translate', text: '언어변경' },
   {
     icon: 'keyboard_arrow_up',
     'icon-alt': 'keyboard_arrow_down',
-    text: 'Labels',
+    text: 'Main Label',
     model: true,
     children: [
-      { icon: 'add', text: 'Create label' }
+      { icon: 'add', text: 'Inner Label_1' },
+      // inner depth 2 동작 안됨 원인 찾기
+      {
+        icon: 'keyboard_arrow_up',
+              'icon-alt': 'keyboard_arrow_down',
+        text: 'Inner Label_1',
+        model: true,
+        children: [
+          { icon: 'add', text: 'Inner Label_2' },
+        ]
+      }
     ]
   },
   {
@@ -24,9 +34,9 @@ const sideBarItems = [
       { text: 'Other contacts' }
     ]
   },
-  { icon: 'settings', text: 'Settings' },
+  { icon: 'settings', text: '사이트 설정 관리' },
   // { icon: 'chat_bubble', text: 'Send feedback' },
-  { icon: 'help', text: 'Help' },
+  { icon: 'help', text: '도움말' },
   // { icon: 'phonelink', text: 'App downloads' },
   // { icon: 'keyboard', text: 'Go to the old version' }
 ]
